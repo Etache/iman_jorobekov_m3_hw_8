c430900cd0f5902f89b477e3a148bd86e54f6787
Etache <nout01010@gmail.com>
2023-03-09 11:27:55 +0600

    2nd commit

--- app/src/main/java/com/example/iman_jorobekov_m3_hw_8/ThirdFragment.kt
+++ app/src/main/java/com/example/iman_jorobekov_m3_hw_8/ThirdFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.iman_jorobekov_m3_hw_8.databinding.FragmentSecondBinding
 import com.example.iman_jorobekov_m3_hw_8.databinding.FragmentThirdBinding
 
 class ThirdFragment : Fragment() {
@@ -22,7 +21,7 @@ class ThirdFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
         val textFromFirstFragment = arguments?.getString("firstFragmentKey")
         val textFromSecondFragment = arguments?.getString("secondFragmentKey")
 
